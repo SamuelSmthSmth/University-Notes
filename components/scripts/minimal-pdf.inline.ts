457649fce79838fecdff6b9ee52ae93f1eacbbde
SamuelSmthSmth <skelechi07@gmail.com>
2026-02-03 04:10:24 +0000

    Quartz sync: Feb 3, 2026, 4:10 AM

---
+++ components/scripts/minimal-pdf.inline.ts
@@ -0,0 +1,8 @@
+document.addEventListener("nav", () => {
+  const pdfs = document.querySelectorAll("iframe[src*='.pdf']")
+  pdfs.forEach((iframe: any) => {
+    if (iframe.src.includes("toolbar=0")) return
+    const separator = iframe.src.includes("#") ? "&" : "#"
+    iframe.src += `${separator}toolbar=0&navpanes=0&scrollbar=0&view=FitH`
+  })
+})
