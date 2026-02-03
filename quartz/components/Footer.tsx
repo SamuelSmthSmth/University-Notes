37b1490aa103fc9a61eaf511d5714b8c68d78caf
SamuelSmthSmth <skelechi07@gmail.com>
2026-02-03 04:19:45 +0000

    Quartz sync: Feb 3, 2026, 4:19 AM

--- quartz/components/Footer.tsx
+++ quartz/components/Footer.tsx
@@ -2,7 +2,6 @@ import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } fro
 import style from "./styles/footer.scss"
 import { version } from "../../package.json"
 import { i18n } from "../i18n"
-import pdfScript from "./scripts/minimal-pdf.inline.ts"
 
 interface Options {
   links: Record<string, string>
@@ -25,7 +24,6 @@ export default ((opts?: Options) => {
             </li>
           ))}
         </ul>
-        <script dangerouslySetInnerHTML={{ __html: pdfScript }} />
       </footer>
     )
   }
